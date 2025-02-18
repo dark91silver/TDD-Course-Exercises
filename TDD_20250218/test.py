810a297f65508ef91d4bf89cf6f6eb4966d08dec
Giulia V <giulia.vergari@gmail.com>
2025-02-18 12:07:46 +0100

    unit test ssearch + unit tests book + skeleton integration test

--- TDD_20250218/test.py
+++ TDD_20250218/test.py
@@ -1,6 +1,7 @@
 import unittest
 
 import booking
+import errors
 
 #test ricerca
 class TestSearch(unittest.TestCase):
@@ -38,33 +39,136 @@ class TestSearch(unittest.TestCase):
     def data_vuota(self):
         arrivo = ""
         partenza = ""
-        self.assertEqual(booking.search(arrivo, partenza, "test.json"), [])
+        self.assertRaises(booking.search(arrivo, partenza, "test.json"), errors.WrongFormatDate)
     #richiesta fallisce perché data formato errato
     def formato_sbagliato(self):
         arrivo = "ventiquattro febbraio 2025"
         partenza = "02 24th 2025"
-        self.assertEqual(booking.search(arrivo, partenza, "test.json"), [])
+        self.assertRaises(booking.search(arrivo, partenza, "test.json"), errors.WrongFormatDate)
     #richiesta fallisce perché data antecedente a oggi
     def ieri(self):
         arrivo = "2025-02-17"
         partenza = "2025-02-18"
-        self.assertEqual(booking.search(arrivo, partenza, "test.json"), [])
+        self.assertEqual(booking.search(arrivo, partenza, "test.json"), errors.WrongIntervalDate)
     #richiesta fallisce perché arrivo dopo partenza
     def invertiti(self):
         arrivo = "2025-02-23"
         partenza = "2025-02-22"
-        self.assertEqual(booking.search(arrivo, partenza, "test.json"), [])
+        self.assertEqual(booking.search(arrivo, partenza, "test.json"), errors.WrongIntervalDate)
 
 #test prenotazione
 class TestBook(unittest.TestCase):
     #prenotazione ok
     def ok(self):
-        pass
+        pren = {
+            "arrivo": "2025-02-24",
+            "partenza": "2025-02-25",
+            "nome": "luigi",
+            "cognome": "bianchi",
+            "cf": "cde234"
+        }
+        self.assertTrue(booking.book(pren, "101", "test.json"))
     #prenotazione fallisce perché qualcuno ha già prenotato quella camera
-    #prenotazione fallisce perché campi nulli o formato errato
+    def bloccata(self):
+        pren = {
+            "arrivo": "2025-02-22",
+            "partenza": "2025-02-23",
+            "nome": "luigi",
+            "cognome": "bianchi",
+            "cf": "cde234"
+        }
+        self.assertFalse(booking.book(pren, "101", "test.json"))
+    #prenotazione fallisce perché campi nulli
+    def formato_errato(self):
+        pren = {
+            "arrivo": "2025-02-22",
+            "partenza": "2025-02-23",
+            "nome": None,
+            "cognome": "bianchi",
+            "cf": "cde234"
+        }
+        self.assertRaises(booking.book(pren, "101", "test.json"), errors.WrongFormatInput)
+    #prenotazione fallisce perché campi vuoti
+    def formato_errato(self):
+        pren = {
+            "arrivo": "2025-02-22",
+            "partenza": "2025-02-23",
+            "nome": "",
+            "cognome": "",
+            "cf": "cde234"
+        }
+        self.assertRaises(booking.book(pren, "101", "test.json"), errors.WrongFormatInput)
+    #prenotazione fallisce perché campi formato errato
+    def formato_errato(self):
+        pren = {
+            "arrivo": "2025-02-22",
+            "partenza": "2025-02-23",
+            "nome": "luigi",
+            "cognome": "bianchi",
+            "cf": 12345
+        }
+        self.assertRaises(booking.book(pren, "101", "test.json"), errors.WrongFormatInput)
     #prenotazione fallisce perché date formato errato
+    def date_errate(self):
+        pren = {
+            "arrivo": "duemilaventicinque trattino zerodue trattino diciotto",
+            "partenza": "",
+            "nome": "luigi",
+            "cognome": "bianchi",
+            "cf": "cde234"
+        }
+        self.assertRaises(booking.book(pren, "101", "test.json"), errors.WrongFormatDate)
     #prenotazione fallisce perché date invertite
-    #prenotazione fallisce perché utente ha prenotato stessa camera per stessi giorni
+    def date_errate(self):
+        pren = {
+            "arrivo": "2025-02-25",
+            "partenza": "2025-02-24",
+            "nome": "luigi",
+            "cognome": "bianchi",
+            "cf": "cde234"
+        }
+        self.assertRaises(booking.book(pren, "101", "test.json"), errors.WrongIntervalDate)
+    #prenotazione fallisce perché ieri
+    def date_errate(self):
+        pren = {
+            "arrivo": "2025-02-17",
+            "partenza": "2025-02-18",
+            "nome": "luigi",
+            "cognome": "bianchi",
+            "cf": "cde234"
+        }
+        self.assertRaises(booking.book(pren, "101", "test.json"), errors.WrongIntervalDate)
+    #prenotazione fallisce perché stesso utente ha prenotato stessa camera per stessi giorni
+    def bloccata(self):
+        pren = {
+            "arrivo": "2025-02-20",
+            "partenza": "2025-02-24",
+            "nome": "mario",
+            "cognome": "rossi",
+            "cf": "abc123"
+        }
+        self.assertFalse(booking.book(pren, "101", "test.json"))
+
+class IntegrationTest(unittest.TestCase):
+    def ok(self):
+        #utente fa ricerca
+        #ricerca dà esito positivo
+        #utente prenota
+        #prenotazione dà esito positivo
+        pass
+    def no_disponibilità(self):
+        #utente fa ricerca
+        #ricerca dà esito negativo
+        pass
+    def prenotazione_concorrente(self):
+        #utente1 fa ricerca
+        #utente2 fa ricerca
+        #entrambi esito positivo
+        #utente1 prenota
+        #utente1 ha esito positivo
+        #utente2 prenota
+        #utente2 ha esito negativo
+        pass
 
 if __name__ == '__main__':
     unittest.main()
